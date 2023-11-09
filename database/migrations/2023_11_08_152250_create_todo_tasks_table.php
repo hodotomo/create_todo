22599b2aa53b706e512e9d82335948dc59ccf40a
hodotomo <thimoronpa@ezweb.ne.jp>
2023-11-09 02:07:58 +0000

    migration

---
+++ database/migrations/2023_11_08_152250_create_todo_tasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todo_tasks', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained();
+            $table->id('selection_id');
+            $table->boolean('necessity');
+            $table->boolean('complete_flag');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todo_tasks');
+    }
+};
